f74583dcb19593c45ff84db772135a65dfcc72a5
Sourabh <sourabh6712@gmail.com>
2025-04-29 12:52:04 +0530

    route route added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
-const authRoutes = require('./routes/authRoutes');
-const expenseRoutes = require('./routes/expanseRoutes');
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expanseRoutes");
 
 dotenv.config();
 connectDB();
@@ -13,10 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/expenses', expenseRoutes);
-
-
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
